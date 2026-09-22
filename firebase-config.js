f17ecb08f3e2e8abb0d4c1b862d4d7c77079cfb0
gallinari2307-ux <gallinari2307@gmail.com>
2026-09-22 13:05:35 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAyXbwcXVX9GQVxS1phpkJSDDbepxG81tc",
+  authDomain: "site-5dfa8.firebaseapp.com",
+  projectId: "site-5dfa8",
+  storageBucket: "site-5dfa8.firebasestorage.app",
+  messagingSenderId: "930402051551",
+  appId: "1:930402051551:web:9ecb2bf248acc9c5a5506a",
+  measurementId: "G-SLRERNNQJD"
+};
